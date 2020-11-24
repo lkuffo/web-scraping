932c5c85c11dfa5ce94c160c6bb13f1e174e3d34
lkuffo <lkuffo@espol.edu.ec>
2020-11-23 23:54:00 -0500

    Nuevo chromedriver 87 y algunas actualizaciones

--- NIVEL 3/1_olx.py
+++ NIVEL 3/1_olx.py
@@ -4,23 +4,40 @@ OBJETIVO:
     - Aprender a realizar extracciones que requieran una accion de click para cargar datos.
     - Introducirnos a la logica de Selenium
 CREADO POR: LEONARDO KUFFO
-ULTIMA VEZ EDITADO: 16 ABRIL 2020
+ULTIMA VEZ EDITADO: 23 NOVIEMBRE 2020
 """
+
+#####
+### ATENCION: OLX necesita que le demos permisos de geolocalizacion al navegador de selenium para que nos muestre los datos
+### Esto lo haremos una unica vez en la primer corrida del programa
+#####
 import random
 from time import sleep
 from selenium import webdriver # pip install selenium
+from selenium.webdriver.chrome.options import Options
 
 # Instancio el driver de selenium que va a controlar el navegador
 # A partir de este objeto voy a realizar el web scraping e interacciones
-driver = webdriver.Chrome('./chromedriver') # REMPLAZA AQUI EL NOMBRE DE TU CHROME DRIVER
+driver = webdriver.Chrome('./chromedriver', chrome_options=opts) # REMPLAZA AQUI EL NOMBRE DE TU CHROME DRIVER
 
 # Voy a la pagina que requiero
 driver.get('https://www.olx.com.ec/autos_c378')
-driver.refresh() # Solucion de un bug extraño en donde los anuncios solo cargan al hacerle refresh o al darle click a algun elemento
 
 sleep(5) # Esperamos que cargue el boton
 # Busco el boton para cargar mas informacion
-
+boton = driver.find_element_by_xpath('//button[@data-aut-id="btnLoadMore"]')
+for i in range(3): # Voy a darle click en cargar mas 3 veces
+    try:
+        # le doy click
+        boton.click()
+        # espero que cargue la informacion dinamica
+        sleep(random.uniform(8.0, 10.0))
+        # busco el boton nuevamente para darle click en la siguiente iteracion
+        boton = driver.find_element_by_xpath('//button[@data-aut-id="btnLoadMore"]')
+    except:
+        # si hay algun error, rompo el lazo. No me complico.
+        break
+    
 # Encuentro cual es el XPATH de cada elemento donde esta la informacion que quiero extraer
 # Esto es una LISTA. Por eso el metodo esta en plural
 autos = driver.find_elements_by_xpath('//li[@data-aut-id="itemBox"]')
