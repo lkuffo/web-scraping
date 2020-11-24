ff237266fa8d64fc50e2e9860888f5018b9b2781
lkuffo <lkuffo@espol.edu.ec>
2020-11-24 00:01:49 -0500

    OLX FIX

--- NIVEL 3/1_olx.py
+++ NIVEL 3/1_olx.py
@@ -9,20 +9,19 @@ ULTIMA VEZ EDITADO: 23 NOVIEMBRE 2020
 
 #####
 ### ATENCION: OLX necesita que le demos permisos de geolocalizacion al navegador de selenium para que nos muestre los datos
-### Esto lo haremos una unica vez en la primer corrida del programa
+### Esto lo haremos una unica vez en la primer corrida del programa. Este problema es mas comun en usuarios de MAC
 #####
 import random
 from time import sleep
 from selenium import webdriver # pip install selenium
-from selenium.webdriver.chrome.options import Options
 
 # Instancio el driver de selenium que va a controlar el navegador
 # A partir de este objeto voy a realizar el web scraping e interacciones
-driver = webdriver.Chrome('./chromedriver', chrome_options=opts) # REMPLAZA AQUI EL NOMBRE DE TU CHROME DRIVER
-
+driver = webdriver.Chrome('./chromedriver') # REMPLAZA AQUI EL NOMBRE DE TU CHROME DRIVER
 # Voy a la pagina que requiero
 driver.get('https://www.olx.com.ec/autos_c378')
-
+sleep(3)
+driver.refresh() # Solucion de un bug extraño en Windows en donde los anuncios solo cargan al hacerle refresh o al darle click a algun elemento
 sleep(5) # Esperamos que cargue el boton
 # Busco el boton para cargar mas informacion
 boton = driver.find_element_by_xpath('//button[@data-aut-id="btnLoadMore"]')
@@ -37,7 +36,7 @@ for i in range(3): # Voy a darle click en cargar mas 3 veces
     except:
         # si hay algun error, rompo el lazo. No me complico.
         break
-    
+
 # Encuentro cual es el XPATH de cada elemento donde esta la informacion que quiero extraer
 # Esto es una LISTA. Por eso el metodo esta en plural
 autos = driver.find_elements_by_xpath('//li[@data-aut-id="itemBox"]')
