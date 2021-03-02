99c872213e8fa365d45b4a2194b7452a519760bc
lkuffo <lkuffo@espol.edu.ec>
2021-03-02 01:43:21 -0500

    [2021 03] Actualizaciones y Nuevas Extracciones

--- NIVEL 2/5_farmaciacruzverde.py
+++ NIVEL 2/5_farmaciacruzverde.py
@@ -1,13 +1,20 @@
+"""	
+OBJETIVO: 	
+    - Aprender a utilizar Link Extractor de una manera mas sofisticada	
+CREADO POR: LEONARDO KUFFO	
+ULTIMA VEZ EDITADO: 2 marzo 2021
+"""
 from scrapy.item import Field
 from scrapy.item import Item
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.selector import Selector
 from scrapy.linkextractors import LinkExtractor
+from scrapy.loader.processors import MapCompose
 from scrapy.loader import ItemLoader
  
 class Farmacia(Item):
-    nombre = Field()
-    precio = Field()
+    Nombre = Field()
+    Precio = Field()
  
  
 class CruzVerdeSpider(CrawlSpider):
@@ -36,7 +43,7 @@ class CruzVerdeSpider(CrawlSpider):
  
         for producto in productos:
             item= ItemLoader(Farmacia(), producto)
-            item.add_xpath('nombre','.//div[@class="pdp-link"]/a/text()')
-            #item.add_xpath('precio','.//div[contains(@class, "large-price")]//span[@class="value"]/text()')
+            item.add_xpath('Nombre', './/div[@class="tile-body px-3 pt-3 pb-0 d-flex flex-column pb-0"]//div[@class="pdp-link"]/a/text()')
+            item.add_xpath('Precio', './/span[contains(@class, "value ")]/text()')
  
             yield item.load_item()
